4dc54396fdcf8036081c71921c947727c4de0b28
Deepak Avadhani <87537653+DeepakAvadhani@users.noreply.github.com>
2025-02-02 21:20:37 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const cors = require("cors");
 const productRoute = require("./routes/product.route.js");
 const app = express();
 app.use(express.json());
+app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use("/api/products", productRoute);
 app.get("/", (req, res) => {
